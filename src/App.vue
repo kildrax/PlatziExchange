34435096ba3edbc1e2abaf706f9d68d19f61b89a
kildrax <jcbest15@hotmail.com>
2021-03-26 15:56:36 -0500

    First push

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <px-header />
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import PxHeader from "./components/PxHeader.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: { PxHeader },
 };
 </script>
 
